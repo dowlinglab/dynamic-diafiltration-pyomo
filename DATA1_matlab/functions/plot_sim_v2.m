bbd1da6c1cb634107fbdf76552c2362567ac4d03
keshavkasturirangan <kkasturi@nd.edu>
2025-07-30 18:47:55 -0400

    Displays Figs. 2, 3 correctly and Fig. 4 incorrectly
    This version displays figures 2 and 3. Figure 4 currently has multiple plots overlaid and requires an update of the MATLAB, which will be addressed in the next version.

---
+++ DATA1_matlab/functions/plot_sim_v2.m
@@ -0,0 +1,126 @@
+function plot_sim_v2(data_stru, model_stru, sim_stru, opts)
+
+% Function to plot simulation results against experimental data
+
+% Inputs:
+%   - data_stru - structured experimental data
+%   - model_stru - structure with partial model configuration (e.g., title, file names)
+%   - sim_stru - structure with simulation results (time, concentration, mass)
+%   - opts - options structure controlling plot behavior
+
+% Returns:
+%     Mass data/prediction comparison plot
+%     Concentration data/prediction comparison plot
+
+% Plot mass data/prediction comparison
+
+% If opts is not provided, initialize with empty struct
+if nargin < 4
+    opts = struct();
+end
+
+% Set default behavior for saving, showing, and layout
+if ~isfield(opts, 'do_save'), opts.do_save = true; end            % Save plots to file?
+if ~isfield(opts, 'do_show'), opts.do_show = true; end            % Show figures?
+if ~isfield(opts, 'subplot_mode'), opts.subplot_mode = false; end % Use subplot layout?
+if ~isfield(opts, 'fig_id_mass'), opts.fig_id_mass = figure; end  % Mass plot figure handle
+
+% Compute delay from experiment start
+t_delay = data_stru.data_raw(1).time(1);
+n_vials = data_stru.data_config.n;     % Number of vials
+n_comp = data_stru.data_config.nc;     % Number of chemical components
+
+% === MASS PLOT ===
+if opts.do_show
+    figure(opts.fig_id_mass); % Activate the specified figure for mass plot
+end
+hold on
+
+% Plot experimental and simulated mass for each vial
+for i = 1:n_vials
+    plot(data_stru.data_raw(i).time - t_delay, data_stru.data_raw(i).mass, 'r.');     % Experimental data
+    plot(sim_stru(i).time - t_delay, sim_stru(i).mV, 'b-', 'LineWidth', 2);           % Simulated data
+end
+
+% Label and format mass plot
+xlabel('Time [s]', 'FontSize', 15)
+ylabel('Collected Permeate Mass [g]', 'FontSize', 15)
+legend({'Measurements', 'Predictions'}, 'FontSize', 12, 'Location', 'Best')
+title({'\fontsize{15} Mass Predictions', ['\fontsize{11}' model_stru.titlestr]}, 'interpreter', 'tex')
+set(gca, 'FontSize', 12)
+hold off
+
+% Save figure if enabled
+if opts.do_save
+    saveas(gcf, fullfile(model_stru.filenamestr, ['mass-', model_stru.filenamestr, '.png']));
+end
+
+% === CONCENTRATION PLOTS ===
+% plot concentration data/prediction comparison
+% loop over components
+
+for j = 1:n_comp
+    % Determine layout for concentration plot
+    if opts.subplot_mode
+        if j == 1
+            fig_conc = figure; % Create new figure for subplot
+        end
+        subplot(n_comp, 1, j); % Create subplot for each component
+    elseif isfield(opts, 'fig_id_conc')
+        figure(opts.fig_id_conc(j)); % Use external figure handle
+    else
+        figure; % Create new figure per component
+    end
+
+    hold on
+
+    % Plot retentate concentration measurements (if available)
+    if isfield(data_stru.data_raw(end), 'cF_exp')
+        for i = 1:n_vials % retentate concentration measurements
+            if ~isempty(data_stru.data_raw(i).cF_exp) % if retentate concentration is measured for each vial
+                if size(data_stru.data_raw(i).cF_exp, 1) == 1 % one measurement for each vial
+                    % One data point
+                    plot(data_stru.data_raw(i).time(end) - t_delay, data_stru.data_raw(i).cF_exp(j), ...
+                        'ms', 'MarkerFaceColor', 'm', 'MarkerSize', 10); % continous measurements
+                else
+                    % Time-series data
+                    plot(data_stru.data_raw(i).time - t_delay, data_stru.data_raw(i).cF_exp(:, j), ...
+                        'mo', 'MarkerFaceColor', 'w', 'MarkerSize', 5);
+                end
+            end
+        end
+    end
+
+    % Plot predicted concentrations and vial data
+    for i = 1:n_vials
+        plot(sim_stru(i).time - t_delay, sim_stru(i).cF(:, j), 'g-', 'LineWidth', 2); % Retentate conc. prediction
+        scatter(sim_stru(i).time(end) - t_delay, sim_stru(i).cV(end, j), 100, ...      % Vial concentration prediction
+            'ks', 'MarkerFaceColor', 'k', 'MarkerFaceAlpha', 0.2);
+
+        if isfield(sim_stru, 'cH')
+            plot(sim_stru(i).time - t_delay, sim_stru(i).cH(:, j), 'k-', 'LineWidth', 2); % Optional: permeate concentration
+        end
+
+        scatter(data_stru.data_raw(i).time(end) - t_delay, data_stru.data_raw(i).cV_avg(j), 100, ... % Measured vial concentration
+            'cs', 'MarkerFaceColor', 'c', 'MarkerFaceAlpha', 0.2);
+    end
+
+    % Initial concentrations before and after overflow
+    plot(0 - t_delay, data_stru.data_config.C_F0(j), 'ms', 'MarkerFaceColor', 'm', 'MarkerSize', 10);
+    plot(0, model_stru.initialization.C_F0(j), 'ms', 'MarkerFaceColor', 'm', 'MarkerSize', 10);
+
+    % Axis labels and title
+    xlabel('Time [s]', 'FontSize', 15)
+    ylabel(['Concentration of ', strtrim(data_stru.data_config.namec(j,:)), ' [mM]'], 'FontSize', 15)
+    title({'\fontsize{15} Concentration Predictions', ['\fontsize{11}' model_stru.titlestr]}, 'interpreter', 'tex')
+    set(gca, 'FontSize', 12)
+    hold off
+
+    % Save concentration plot if enabled
+    if opts.do_save && ~opts.subplot_mode
+        saveas(gcf, fullfile(model_stru.filenamestr, ...
+            ['concentration_', strtrim(data_stru.data_config.namec(j,:)), '-', model_stru.filenamestr, '.png']));
+    end
+end
+end
+
--- DATA1_matlab/test_v1_KKR.py
+++ DATA1_matlab/test_v1_KKR.py
@@ -1,4 +1,5 @@
 # %% This code plots but does not process any data. This code uses data that has been been processed in MATLAB 
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as spio
@@ -8,16 +9,97 @@ from matplotlib.lines import Line2D
 from matplotlib import patheffects
 import pandas as pd
 
-from diafiltration_plots import loadmat
-from diafiltration_plots import plot_sim_comparison
+from diafiltration_plots import loadmat # function that loads experimental and simulated data
+from diafiltration_plots import plot_sim_comparison # function to generate Fig. 2
 from diafiltration_plots import plot_contour
 from diafiltration_plots import plot_sim
 from diafiltration_plots import plot_sim_show
+from diafiltration_plots import plot_conc_range # function to generate Fig. 3
 
-# %% filtration parameter estimation(hybrid) using the data file 'data_stru-dataset501.1.mat'
-data_stru_f_501_1 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/data_stru-dataset501.1.mat')['data_stru']
-fit_stru_501_1 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.1 concpolar/fit_stru.mat')['fit_stru']
-plot_sim_comparison(data_stru_f_501_1,fit_stru_501_1,plot_pred=True,cond=True,lg=False)
+# %% To run the files associated with this code, either run the code as written
+#    if you are certain that the directory/folder that the code is stored and 
+#    run in is the same as where the 'data' folder is stored.
+#    If you are uncertain of this or if teh code resides in a different location
+#    provide the full path to the 'data' folder location.
+#    For example: r'/Users/user_name/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data/data_stru-dataset501.1.mat'
+
+
+# %% Figure 2: Diafiltration (dataset used --> 'data_stru-dataset511.12.mat') 
+#    and filtration (dataset used --> 'data_stru-dataset501.1.mat') mass and concentration profile
+
+#    Filtration
+# data_stru_f_501_1 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data/data_stru-dataset501.1.mat')['data_stru']
+data_stru_f_501_1 = loadmat(r'./data/data_stru-dataset501.1.mat')['data_stru']
+# fit_stru_501_1 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data/501.1 concpolar/fit_stru.mat')['fit_stru']
+fit_stru_f_501_1 = loadmat(r'./data/501.1 concpolar/fit_stru.mat')['fit_stru']
+
+plot_sim_comparison(data_stru_f_501_1, fit_stru_f_501_1, plot_pred = True, cond = True, lg = False)
+
+#    Diafiltration
+# data_stru_d_511_12 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/data_stru-dataset511.12.mat')['data_stru']
+data_stru_d_511_12 = loadmat(r'./data/data_stru-dataset511.12.mat')['data_stru']
+# fit_stru_d_511_12 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/511.12 concpolar/fit_stru.mat')['fit_stru']
+fit_stru_d_511_12 = loadmat(r'./data/511.12 concpolar/fit_stru.mat')['fit_stru']
+
+plot_sim_comparison(data_stru_d_511_12, fit_stru_d_511_12, plot_pred = True, cond = True, lg = False)
+
+# %% Figure 3: Diafiltration (dataset used --> 'diafiltration.csv') 
+#    and filtration (dataset used --> 'filtration.csv') phase space 
+#    retentate vs. permeate concentration profile
+
+# df_fil_ret_vs_per_conc = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data/experiment space/filtration.csv',header=2)
+df_fil_ret_vs_per_conc = pd.read_csv(r'./data/experiment space/filtration.csv',header=2)
+# df_diafil_ret_vs_per_conc = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data/experiment space/diafiltration.csv',header=2)
+df_diafil_ret_vs_per_conc = pd.read_csv(r'./data/experiment space/diafiltration.csv',header=2)
+
+plot_conc_range(df_fil_ret_vs_per_conc, df_diafil_ret_vs_per_conc) 
+
+# %% Figure 4: Sigma sensitivity analysis with membrane M1 (501.1)
+
+# filtration sigma sensitivity
+# plt.figure()
+
+sigma_f = [0.1,0.5,0.9]
+colorstring_f = 'rbg'
+
+# sim_stru_f_sigma_sens_501_1_sig_0_1 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.1.mat')['sim_stru']
+sim_stru_f_sigma_sens_501_1_sig_0_1 = loadmat(r'./data/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.1.mat')['sim_stru']
+plot_sim(sim_stru_f_sigma_sens_501_1_sig_0_1, colorstring_f[0],'dashed')
+
+# plt.figure()
+
+# sim_stru_f_sigma_sens_501_1_sig_0_5 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.5.mat')['sim_stru']
+sim_stru_f_sigma_sens_501_1_sig_0_5 = loadmat(r'./data/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.5.mat')['sim_stru']
+plot_sim(sim_stru_f_sigma_sens_501_1_sig_0_5, colorstring_f[1],'solid')
+
+# plt.figure()
+
+# sim_stru_f_sigma_sens_501_1_sig_0_9 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.9.mat')['sim_stru']
+sim_stru_f_sigma_sens_501_1_sig_0_9 = loadmat(r'./data/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.9.mat')['sim_stru']
+plot_sim(sim_stru_f_sigma_sens_501_1_sig_0_9, colorstring_f[2],'dotted')
+
+plot_sim_show(sigma_f,colorstring_f)
+
+# plt.figure()
+
+# diafiltration sigma sensitivity
+sigma_d = [0.1,0.5,0.9]
+colorstring_d = 'rbg'
+
+sim_stru_d_sigma_sens_511_12_sig_0_1 = loadmat(r'./data/sigma sensitivity/sim_stru-dat511.12 C_Fin15.2052sig0.1.mat')['sim_stru']
+plot_sim(sim_stru_d_sigma_sens_511_12_sig_0_1, colorstring_d[0],'dashed')
+
+# plt.figure()
+
+sim_stru_d_sigma_sens_511_12_sig_0_5  = loadmat(r'./data/sigma sensitivity/sim_stru-dat511.12 C_Fin15.2052sig0.5.mat')['sim_stru']
+plot_sim(sim_stru_d_sigma_sens_511_12_sig_0_5, colorstring_d[1],'solid')
+
+# plt.figure()
+
+sim_stru_d_sigma_sens_511_12_sig_0_9 = loadmat(r'./data/sigma sensitivity/sim_stru-dat511.12 C_Fin15.2052sig0.9.mat')['sim_stru']
+plot_sim(sim_stru_d_sigma_sens_511_12_sig_0_9, colorstring_d[2],'dotted')
+
+plot_sim_show(sigma_d, colorstring_d)
 
 df = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.1 concpolar/contourdata-x_B-y_Lp.csv')
 plot_contour(df)
@@ -35,40 +117,40 @@ df = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matl
 plot_contour(df)
 plt.show()
 
-# # %% filtration parameter estimation(hybrid) using the data file 'data_stru-dataset501.11.mat'
+# %% filtration parameter estimation(hybrid) using the data file 'data_stru-dataset501.11.mat'
+data_stru_f_501_11 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/data_stru-dataset501.11.mat')['data_stru']
+fit_stru = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11 concpolar/fit_stru.mat')['fit_stru']
+plot_sim_comparison(data_stru_f_501_11,fit_stru,plot_pred=True,cond=True,lg=False)
+
+df = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11 concpolar/contourdata-x_B-y_Lp.csv')
+plot_contour(df)
+plt.show()
+
+df = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11 concpolar/contourdata-x_sigma-y_Lp.csv')
+plot_contour(df)
+plt.show()
+
+# filtration parameter estimation(hybrid)
 # data_stru_f_501_11 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/data_stru-dataset501.11.mat')['data_stru']
-# fit_stru = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11 concpolar/fit_stru.mat')['fit_stru']
-# plot_sim_comparison(data_stru_f_501_11,fit_stru,plot_pred=True,cond=True,lg=False)
+fit_stru = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11/fit_stru.mat')['fit_stru']
+plot_sim_comparison(data_stru_f_501_11,fit_stru,plot_pred=True,cond=False,lg=False)
 
-# df = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11 concpolar/contourdata-x_B-y_Lp.csv')
-# plot_contour(df)
-# plt.show()
+df = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11/contourdata-x_B-y_Lp.csv')
+plot_contour(df)
+plt.show()
 
-# df = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11 concpolar/contourdata-x_sigma-y_Lp.csv')
-# plot_contour(df)
-# plt.show()
+df = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11/contourdata-x_sigma-y_Lp.csv')
+plot_contour(df)
+plt.show()
 
-# # filtration parameter estimation(hybrid)
-# # data_stru_f_501_11 = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/data_stru-dataset501.11.mat')['data_stru']
-# fit_stru = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11/fit_stru.mat')['fit_stru']
-# plot_sim_comparison(data_stru_f_501_11,fit_stru,plot_pred=True,cond=False,lg=False)
+# %% filtration sigma sensitivity
+sigma = [0.1,0.5,0.9]
+colorstring = 'rbg'
 
-# df = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11/contourdata-x_B-y_Lp.csv')
-# plot_contour(df)
-# plt.show()
-
-# df = pd.read_csv(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/501.11/contourdata-x_sigma-y_Lp.csv')
-# plot_contour(df)
-# plt.show()
-
-# # %% filtration sigma sensitivity
-# sigma = [0.1,0.5,0.9]
-# colorstring = 'rbg'
-
-# sim_stru = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.1.mat')['sim_stru']
-# plot_sim(sim_stru,colorstring[0],'dashed')
-# sim_stru = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.5.mat')['sim_stru']
-# plot_sim(sim_stru,colorstring[1],'solid')
-# sim_stru = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.9.mat')['sim_stru']
-# plot_sim(sim_stru,colorstring[2],'dotted')
-# plot_sim_show(sigma,colorstring)
+sim_stru = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.1.mat')['sim_stru']
+plot_sim(sim_stru,colorstring[0],'dashed')
+sim_stru = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.5.mat')['sim_stru']
+plot_sim(sim_stru,colorstring[1],'solid')
+sim_stru = loadmat(r'/Users/kkasturi/GitHub/dynamic-diafiltration-pyomo/DATA1_matlab/data_library/sigma sensitivity/sim_stru-dat501.1 C_Fin5.2843sig0.9.mat')['sim_stru']
+plot_sim(sim_stru,colorstring[2],'dotted')
+plot_sim_show(sigma,colorstring)
